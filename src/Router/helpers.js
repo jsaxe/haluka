48f67a2fd4f4c432c0aa3681f76bc75f5751802a
Robin Panta <hacktivistic@gmail.com>
2019-01-21 17:41:39 +0530

    [feature] router helper

---
+++ src/Router/helpers.js
@@ -0,0 +1,36 @@
+'use strict'
+
+// Dependencies
+
+var Helpers = module.exports = {}
+var routes = []
+
+Helpers.registerGlobalRoutingHelpers = function (controllerPath) {
+	global.to = function (controller) {
+		return require(require('path').join(controllerPath, controller))
+	}
+}
+
+Helpers.pointRoute = function (path, controller) {
+	routes.push({
+		path: path,
+		controller: controller
+	})
+}
+
+Helpers.mapRoutes = function (app) {
+	routes.map((route, idx) => {
+		if (!route.controller) {
+			route.controller = (req, res) => { res.end('Invalid Controller Method provided for this route.') }
+		}else{
+			if (Object.getPrototypeOf(route.controller) !== Object.getPrototypeOf(require('express').Router()))
+		 		route.controller = (req, res) => { res.end('Invalid Controller. Router not exported! Tip: Did you forget `module.exports = router`') }
+
+		}
+		app.use(route.path, route.controller)
+	})
+}
+
+Helpers.getRoutes = function () {
+	return routes
+}
