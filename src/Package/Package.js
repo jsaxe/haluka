d130483ff4c5b205f82c12128d2a27fdc19a57bd
Robin Panta <hacktivistic@gmail.com>
2019-02-09 13:19:24 +0530

    [refactor] package config file name changed

--- src/Package/Package.js
+++ src/Package/Package.js
@@ -25,21 +25,21 @@ Package.load = function(basePath) {
 	var halukaDir = path.join(basePath, 'haluka_packages')
 	return (name) => {
 		var packageDir = path.join(halukaDir, name)
-		var packageFile = path.join(packageDir, 'haluka.json')
+		var packageFile = path.join(packageDir, 'package.json')
 		if (fs.existsSync(packageFile)) {
 			var packageConfig = require(packageFile)
-			var setup = packageConfig.setup
+			var setup = packageConfig.main
 			if (setup === undefined)
-				throw new Error('Invalid Package Setup Script for `' + name + '`.')
+				throw new Error('Invalid Package Main Script for `' + name + '`.')
 			var setupFile = path.join(packageDir, setup)
 			if (fs.existsSync(setupFile)) {
 				var setup = require(setupFile)
 				return setup
 			}else{
-				throw new Error('Setup file `' + setup + '` not found in `' + name + '` package')
+				throw new Error('Main file `' + setup + '` not found in `' + name + '` package')
 			}
 		}else{
-			throw new Error('Invalid Package. `haluka.json` file not found in package root of `' + name + '`.')
+			throw new Error('Invalid Package. `package.json` file not found in package root of `' + name + '`.')
 		}
 
 	}
