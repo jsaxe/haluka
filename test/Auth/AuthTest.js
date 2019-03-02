1c0bfe0a3d17d5bf10762c71fb30ba150316936e
Robin Panta <hacktivistic@gmail.com>
2019-03-03 01:10:34 +0530

    Test authentication middleware

--- test/Auth/AuthTest.js
+++ test/Auth/AuthTest.js
@@ -1,13 +1,63 @@
 'use strict'
 
+var expect = require('chai').expect
 var Auth = require('../../src/Auth/Auth')
+var request = require('supertest')
 
 describe('Auth', function () {
 
+	let auth
 	describe('constructor', function () {
 
 		it('should setup auth', function () {
-			new Auth(app)
+			auth = new Auth(app)
+		})
+
+	})
+
+	describe('AuthenticationMiddleware()', function () {
+
+		it('should return authentication checking express middleware', function (done) {
+
+			var isAuthenticated = auth.AuthenticationMiddleware('/login')
+			expect(isAuthenticated).to.be.instanceOf(Function)
+
+			var haluka = require('../integration/index')
+			haluka.getExpress().get('/secret', isAuthenticated, (req, res) => {
+				res.end('Secret!')
+			})
+			request(haluka.getExpress())
+				.get('/secret')
+				.expect(302, function(err, res) {
+					expect(res.headers.location).to.equal('/login')
+					done()
+				})
+			
+		})
+
+		it('should redirect if logged in', function (done) {
+
+			var isAuthenticated = auth.AuthenticationMiddleware('/login')
+			expect(isAuthenticated).to.be.instanceOf(Function)
+
+			var haluka = require('../integration/index')
+
+			var express = haluka.getExpress()
+
+			express.use((req, res, next) => {
+				req.user = { name: 'Tester'}
+				req.isAuthenticated = () => {
+					return true
+				}
+				next()
+			})
+			express.get('/secret2', isAuthenticated, (req, res) => {
+				res.end('SecretAgain!')
+			})
+			request(express)
+				.get('/secret2')
+				.expect('SecretAgain!', done)
+
 		})
 
 	})
@@ -32,6 +82,12 @@ let app = {
 			}
 		}
 	},
-	getExpress: () => {},
+	getExpress: () => {
+		return {
+			use: (closure) => {
+				closure()
+			}
+		}
+	},
 	map: () => {}
 }
